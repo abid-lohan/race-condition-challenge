d8bc6bffdff4b151060610519b712136e592d4ef
abid-lohan <abidlohan@gmail.com>
2024-06-21 18:34:54 -0300

    Tapa no design parte 3

--- populate.py
+++ populate.py
@@ -13,7 +13,7 @@ def create_users():
     print("Usuários criados com sucesso.")
 
 def create_burgers():
-    burger1 = Burger(name='Hangúrguer de Siri', price=5.00, image='https://media.discordapp.net/attachments/888101596796452937/1253732999909085264/siri.png?ex=6676ed58&is=66759bd8&hm=57a74a8b02e38c1c684872866a7d32379d1c28bf9d9b48bd149c95b187555c3a&=&format=webp&quality=lossless')
+    burger1 = Burger(name='Hanbúrguer de Siri', price=5.00, image='https://media.discordapp.net/attachments/888101596796452937/1253732999909085264/siri.png?ex=6676ed58&is=66759bd8&hm=57a74a8b02e38c1c684872866a7d32379d1c28bf9d9b48bd149c95b187555c3a&=&format=webp&quality=lossless')
     burger2 = Burger(name='Hambúrguer Grisalho', price=1337.00, image='https://media.discordapp.net/attachments/888101596796452937/1253732999669878874/grisalho.png?ex=6676ed58&is=66759bd8&hm=b395d2f360496a472bc4ab6052b52c63e82ab9faf58bd6f65e73577bcb6dae2a&=&format=webp&quality=lossless')
 
     db.session.add(burger1)
